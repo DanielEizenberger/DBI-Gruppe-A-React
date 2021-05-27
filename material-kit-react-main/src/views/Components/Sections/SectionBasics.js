b0fc765aa8b5738e228db78508b3d9ad12b8abc9
dlienbacher17 <lienbacherd@outlook.com>
2021-05-27 11:26:54 +0200

    Grid
    Attempt to build a grid

--- material-kit-react-main/src/views/Components/Sections/SectionBasics.js
+++ material-kit-react-main/src/views/Components/Sections/SectionBasics.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
 
 export default function SectionBasics() {
   React.useEffect(() => {});
@@ -12,6 +14,12 @@ export default function SectionBasics() {
           <Button type="button" color="primary">
             Click if u hate Prettier/Prettier
           </Button>
+          <GridContainer>
+            <GridItem>jo</GridItem>
+            <GridItem>jo</GridItem>
+            <GridItem>jo</GridItem>
+            <GridItem>jo</GridItem>
+          </GridContainer>
         </div>
       </div>
     </div>
